7a96095aa2fd58b15857692ca6c5912cbf7d2b90
isnendyankp <isnendyankp@gmail.com>
2023-08-22 10:17:55 +0700

    S7-191:Cr8 question state value

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import { useState } from 'react';
 import data from './data';
 
 const App = () => {
+  const [questions, setQuestions] = useState(data);
   return <h2>Accordion Starter</h2>;
 };
 export default App;
 
-// -
+// - S7-191:Import useState & data
+// - S7-191:Cr8 question state value
