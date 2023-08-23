2ec5f0d286818e825d73c5bdcd079eccf9d9f3c0
isnendyankp <isnendyankp@gmail.com>
2023-08-23 10:10:15 +0700

    S7-191:Pass in Questions on return

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react';
 import data from './data';
 
-const App = () => {
+function App() {
   const [questions, setQuestions] = useState(data);
-  return <h2>Accordion Starter</h2>;
-};
+  return (
+    <main>
+      <Questions questions={questions} />
+    </main>
+  );
+}
 export default App;
 
 // - S7-191:Import useState & data
 // - S7-191:Cr8 question state value
+// - 
