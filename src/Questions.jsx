3f9c27855f74a6eeeff86cbfb2187249c4d93a1a
isnendyankp <isnendyankp@gmail.com>
2023-08-23 10:19:01 +0700

    S7-191:Add question as prop

--- src/Questions.jsx
+++ src/Questions.jsx
@@ -4,8 +4,8 @@ const Questions = ({ questions }) => {
   return (
     <section className="container">
       <h1>Questions</h1>
-      {questions.map(() => { 
-
+      {questions.map((question) => {
+        
       })}
     </section>
   );
@@ -17,3 +17,4 @@ export default Questions
 // - S7-191:Add parameter questions
 // - S7-191:Add section with className contianer & h1 Questions
 // - S7-191:Iterate over question parameter/prop. Call map since its array
+// - S7-191:Add question as prop/parameter
