28385756bd9f6fbece7ac7bb390a0a09b5e7ff70
isnendyankp <isnendyankp@gmail.com>
2023-08-23 10:27:39 +0700

    S7-191:Import useState

--- src/SingleQuestion.jsx
+++ src/SingleQuestion.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 
 const SingleQuestion = () => {
-  return (
-    <div>SingleQuestion</div>
-  )
-}
+  const [showInfo, setShowInfo] = useState(false);
+  return <div>SingleQuestion</div>;
+};
 
-export default SingleQuestion
+export default SingleQuestion;
 
 // - S7-191:Cr8 SingleQuestion component base
+// - S7-191:Import AiOutlineMinus & AiOutlinePlus
+// - S7-191:Cr8 state value showInfo
