e9ee9bc3766e214b7c32ca1fccd82c4183c68392
isnendyankp <isnendyankp@gmail.com>
2023-08-23 10:17:37 +0700

    S7-191:Iterate over question parameter/prop

--- src/Questions.jsx
+++ src/Questions.jsx
@@ -4,6 +4,9 @@ const Questions = ({ questions }) => {
   return (
     <section className="container">
       <h1>Questions</h1>
+      {questions.map(() => { 
+
+      })}
     </section>
   );
 };
@@ -13,3 +16,4 @@ export default Questions
 // - S7-191:Cr8 question component base
 // - S7-191:Add parameter questions
 // - S7-191:Add section with className contianer & h1 Questions
+// - S7-191:Iterate over question parameter/prop. Call map since its array
