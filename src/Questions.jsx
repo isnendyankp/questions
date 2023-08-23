ad0bd306f1fea73309b19bdb80cd6827f7b3bc89
isnendyankp <isnendyankp@gmail.com>
2023-08-23 10:14:55 +0700

    S7-191:Add section with className contianer

--- src/Questions.jsx
+++ src/Questions.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
 const Questions = ({ questions }) => {
-  return <div>Questions</div>;
+  return (
+    <section className="container">
+      <h1>Questions</h1>
+    </section>
+  );
 };
 
 export default Questions
 
 // - S7-191:Cr8 question component base
-// - 
+// - S7-191:Add parameter questions
+// - S7-191:Add section with className contianer & h1 Questions
