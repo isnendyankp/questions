d6063a38a3409b492ef8da0506255a67cd3d69a2
isnendyankp <isnendyankp@gmail.com>
2023-08-24 12:05:00 +0700

    S7-191:Destructure with title & info

--- src/SingleQuestion.jsx
+++ src/SingleQuestion.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 
-const SingleQuestion = () => {
+const SingleQuestion = ({ title, info }) => {
   const [showInfo, setShowInfo] = useState(false);
   return <div>SingleQuestion</div>;
 };
@@ -11,3 +11,4 @@ export default SingleQuestion;
 // - S7-191:Cr8 SingleQuestion component base
 // - S7-191:Import AiOutlineMinus & AiOutlinePlus
 // - S7-191:Cr8 state value showInfo
+// - S7-191:Destructure with title & info
