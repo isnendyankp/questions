eadf25745ebed4b5b806e8f21b4799a6221cca2f
isnendyankp <isnendyankp@gmail.com>
2023-08-24 12:06:15 +0700

    S7-191:Add article with className question

--- src/SingleQuestion.jsx
+++ src/SingleQuestion.jsx
@@ -3,7 +3,9 @@ import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 
 const SingleQuestion = ({ title, info }) => {
   const [showInfo, setShowInfo] = useState(false);
-  return <div>SingleQuestion</div>;
+  return <article className='question'>
+
+  </article>
 };
 
 export default SingleQuestion;
@@ -12,3 +14,4 @@ export default SingleQuestion;
 // - S7-191:Import AiOutlineMinus & AiOutlinePlus
 // - S7-191:Cr8 state value showInfo
 // - S7-191:Destructure with title & info
+// - S7-191:Add article with className question
